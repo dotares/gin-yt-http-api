49534e1e41997936e00a4b2a917498afde012dfc
fibbbonachos <arish2k3@gmail.com>
2023-09-25 15:49:53 +0530

    update: get exports working for the Router server

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	r "api/server"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	r.Router();
 }
--- server/router.go
+++ server/router.go
@@ -1,1 +1,13 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Router() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200 ,"Hello")
+	})
+	r.Run()
+}
