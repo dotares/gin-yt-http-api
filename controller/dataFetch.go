7cbae610868cb2f7ddbde5726b31a35b0f3362f6
fibbbonachos <arish2k3@gmail.com>
2023-09-27 14:39:40 +0530

    update: print the string data of yt in the console

--- controller/dataFetch.go
+++ controller/dataFetch.go
@@ -1,1 +1,26 @@
 package dataFetch
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func DataFetch () {
+	ytURL := "https://www.youtube.com/results?search_query=grenade+audio" 
+
+	res, err := http.Get(ytURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+}
--- server/router.go
+++ server/router.go
@@ -1,13 +1,15 @@
 package router
 
 import (
+	dataFetch "api/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Router() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200 ,"Hello")
+		dataFetch.DataFetch()
 	})
 	r.Run()
 }
