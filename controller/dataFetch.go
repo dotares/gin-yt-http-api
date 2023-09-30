d16f7ac848c8034ca3896bc57f38a0f574dfbd3d
fibbbonachos <arish2k3@gmail.com>
2023-09-30 12:36:11 +0530

    update: get the regex working but not json yet

--- controller/dataFetch.go
+++ controller/dataFetch.go
@@ -25,6 +25,29 @@ func DataFetch () {
 
 	bodyString := string(body)
 
-	re := regexp.MustCompile("(?i)\\{\"videoRenderer\":\\{\"videoId\":\"[^\"]*\"")
-	fmt.Println("******************* regex match:", re.MatchString(bodyString));
+	idRegex := regexp.MustCompile("(?i)\\{\"videoRenderer\":\\{\"videoId\":\"[^\"]*\"")
+	titleRegex := regexp.MustCompile("(?i),\"title\":\\{\"runs\":\\[\\{\"text\":\"[^\"]*\"\\}\\]")
+	artistRegex := regexp.MustCompile("(?i),\"longBylineText\":\\{\"runs\":\\[\\{\"text\":\"[^\"]*\"")
+	coverRegex := regexp.MustCompile("(?i)\\{\"videoId\":\"[^\"]*\",\"thumbnail\":\\{\"thumbnails\":\\[\\{\"url\":\"[^\"]*\",\"width\":360,\"height\":202\\}")
+
+	idMatches := idRegex.FindAllString(bodyString, -1)
+	titleMatches := titleRegex.FindAllString(bodyString, -1)
+	artistMatches := artistRegex.FindAllString(bodyString, -1)
+	coverMatches := coverRegex.FindAllString(bodyString, -1)
+
+	for _, m := range idMatches {
+		fmt.Println(m[len(m)-12:len(m)-1]) 
+	} 
+
+	for _, m := range titleMatches {
+		fmt.Println(m[27:len(m)-3]) 
+	} 
+
+	for _, m := range artistMatches {
+		fmt.Println(m[36:len(m)-1]) 
+	} 
+
+	for _, m := range coverMatches {
+		fmt.Println(m[60:len(m)-27]) 
+	} 
 }
